0f6379513dff2faa88c9800be907640459fd64a0
Dominik Hautau <dominikhautau@gmail.com>
2023-12-13 10:18:28 +0100

    sc session

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledButton = styled.button`
+  background: green;
+  color: white;
+  padding: 10px 20px;
+
+  &:hover {
+    background: lightgreen;
+  }
+`;
+
+const StyledErrorButton = styled(StyledButton)`
+  background: red;
+`;
+
+const StyledLink = styled(Link)`
+  fontsize: 2rem;
+  color: lightcoral;
+`;
+
+const StyledFlexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 5px;
+  background: var(--primary-background);
+`;
+
 export default function HomePage() {
   return (
     <>
-      <h1>Hello from Next.js</h1>
+      <h1>Styled Components</h1>
+      <StyledFlexContainer>
+        <StyledButton
+          type="button"
+          onClick={() => {
+            console.log("Lol");
+          }}
+        >
+          Brokkoli
+        </StyledButton>
+        <StyledErrorButton>Error Button</StyledErrorButton>
+        <StyledLink href="/">Hallo</StyledLink>
+      </StyledFlexContainer>
     </>
   );
 }
--- styles.js
+++ styles.js
@@ -1,21 +1,60 @@
 import { createGlobalStyle } from "styled-components";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export default createGlobalStyle`
-  *, 
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-
   :root {
     --primary-color: #22babb; 
     --secondary-color: #f24405;
     --primary-background: #d3d3d3;
   }
 
-  body {
-    margin: 0;
-    font-family: system-ui;
-    padding: 2rem;
-  }
+/*
+  1. Use a more-intuitive box-sizing model.
+*/
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+/*
+  2. Remove default margin
+*/
+* {
+  margin: 0;
+}
+/*
+  Typographic tweaks!
+  3. Add accessible line-height
+  4. Improve text rendering
+*/
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+  font-family: ${openSans.style.fontFamily}
+}
+/*
+  5. Improve media defaults
+*/
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+/*
+  6. Remove built-in form typography styles
+*/
+input, button, textarea, select {
+  font: inherit;
+}
+/*
+  7. Avoid text overflows
+*/
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+/*
+  8. Create a root stacking context
+*/
+#root, #__next {
+  isolation: isolate;
+}
 `;
